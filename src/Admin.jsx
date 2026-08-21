d292db712f87843f78a167e0ace13f1700572212
Kirill <kirillka.ru@bk.ru>
2026-08-21 20:43:45 +0300

    improve admin orders

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+
 const [orders, setOrders] = useState([]);
 async function changeStatus(orderNumber, status) {
   await fetch("/api/update-order", {
