f5ff95a459a9ed1f9a6963dd28ba99f10c81cc02
Kirill <kirillka.ru@bk.ru>
2026-08-21 08:51:55 +0300

    clean checkout form

--- api/telegram.js
+++ api/telegram.js
@@ -107,7 +107,77 @@ export default async function handler(req, res) {
       },
     );
   }
+  if (data.startsWith("done_")) {
+    const orderNumber = data.replace("done_", "");
 
+    await fetch(
+      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/answerCallbackQuery`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          callback_query_id: query.id,
+          text: `Заказ №${orderNumber} выполнен`,
+        }),
+      },
+    );
+
+    await fetch(
+      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/editMessageReplyMarkup`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: query.message.chat.id,
+          message_id: query.message.message_id,
+
+          reply_markup: {
+            inline_keyboard: [],
+          },
+        }),
+      },
+    );
+  }
+
+  if (data.startsWith("cancel_")) {
+    const orderNumber = data.replace("cancel_", "");
+
+    await fetch(
+      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/answerCallbackQuery`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          callback_query_id: query.id,
+          text: `Заказ №${orderNumber} отменён`,
+        }),
+      },
+    );
+
+    await fetch(
+      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/editMessageReplyMarkup`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: query.message.chat.id,
+          message_id: query.message.message_id,
+
+          reply_markup: {
+            inline_keyboard: [],
+          },
+        }),
+      },
+    );
+  }
   res.json({
     ok: true,
   });
