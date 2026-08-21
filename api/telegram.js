2d4ed93a9edf84d550d251d15020140c24656940
Kirill <kirillka.ru@bk.ru>
2026-08-21 08:49:39 +0300

    clean checkout form

--- api/telegram.js
+++ api/telegram.js
@@ -59,6 +59,54 @@ export default async function handler(req, res) {
       },
     );
   }
+  if (data.startsWith("delivery_")) {
+    const orderNumber = data.replace("delivery_", "");
+
+    await fetch(
+      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/answerCallbackQuery`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          callback_query_id: query.id,
+          text: `Заказ №${orderNumber} передан в доставку`,
+        }),
+      },
+    );
+
+    await fetch(
+      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/editMessageReplyMarkup`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: query.message.chat.id,
+          message_id: query.message.message_id,
+
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "✅ Завершить заказ",
+                  callback_data: `done_${orderNumber}`,
+                },
+              ],
+              [
+                {
+                  text: "❌ Отменить заказ",
+                  callback_data: `cancel_${orderNumber}`,
+                },
+              ],
+            ],
+          },
+        }),
+      },
+    );
+  }
 
   res.json({
     ok: true,
